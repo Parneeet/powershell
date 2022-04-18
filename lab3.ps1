c4550e691196883f344e0477734ac62905aadeeb
Parneeet <200455917@student.georgianc.on.ca>
2022-04-18 10:44:58 -0400

    lab 3 done

--- lab3.ps1
+++ lab3.ps1
@@ -1,3 +1,3 @@
 ﻿get-ciminstance win32_networkadapterconfiguration |
-#? { $_.IPEnabled -Eq "True" } |
+? { $_.IPEnabled -Eq "True" } |
 format-table Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
